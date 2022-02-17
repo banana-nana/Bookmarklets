aa7750b02c4f43ba68e0d797a4582662f55aced9
banana-nana <50461810+banana-nana@users.noreply.github.com>
2022-02-17 10:06:31 -0600

    Update Banana-nana.js

--- Banana-nana.js
+++ Banana-nana.js
@@ -2,14 +2,14 @@ var user1="sillyangel3";
 var username=prompt('Please Log in. Username:',' ');
 if (username==user1){
    var pass1="murded21";
-   password=prompt('If you are suppose to be here you have a password. Please type                  it now:',' ');
+   password=prompt('If you are suppose to be here you have a password. Please type it now:',' ');
     if (password==pass1){
         alert("Welcome Back " + username + ".");
      }
     else {
-  prompt("Wrong");
+  alert("Wrong");
     }
 }
 else {
-  prompt("Wrong");
+  alert("Wrong");
 }
