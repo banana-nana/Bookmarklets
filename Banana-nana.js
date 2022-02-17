b2d00abf523bd6c41becf14c3aa3faa7c3132a7f
banana-nana <50461810+banana-nana@users.noreply.github.com>
2022-02-17 10:50:47 -0600

    Update Banana-nana.js

--- Banana-nana.js
+++ Banana-nana.js
@@ -4,12 +4,8 @@ if (username==user1){
    var pass1="murded21";
    password=prompt('If you are suppose to be here you have a password. Please type it now:',' ');
     if (password==pass1){
+       window.location.href = "about:blank";
         alert("Welcome Back " + username + ".");
-       var iframe = document.createElement('iframe'); 
-       iframe.src='https://banana-nana.github.io'; 
-       iframe.width = window.innerWidth;
-       iframe.height = window.innerHeight;
-       document.body.appendChild(iframe);
      }
     else {
   alert("Wrong");
