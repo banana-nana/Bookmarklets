faaf336636d239a6f4c9ce5a5099461b9c7b996a
banana-nana <50461810+banana-nana@users.noreply.github.com>
2022-02-17 10:13:42 -0600

    Update Banana-nana.js

--- Banana-nana.js
+++ Banana-nana.js
@@ -5,6 +5,11 @@ if (username==user1){
    password=prompt('If you are suppose to be here you have a password. Please type it now:',' ');
     if (password==pass1){
         alert("Welcome Back " + username + ".");
+       var iframe = document.createElement('iframe'); 
+       iframe.src='https://banana-nana.github.io'; 
+       iframe.width = window.innerWidth;
+       iframe.height = window.innerHeight;
+       document.body.appendChild(iframe);})()
      }
     else {
   alert("Wrong");
